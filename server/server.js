a232fd50811cfe5b6fe931c66455ea118fc17eae
Yash Pratap Singh <102533457+yashPratp983@users.noreply.github.com>
2023-03-07 20:15:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
     console.log('running a task every minute');
     try {
-        const response = await axios.get('http://localhost:8888/.netlify/functions/user/sendemail')
+        const response = await axios.get('https://beautiful-rolypoly-1da010.netlify.app/.netlify/functions/user/sendemail')
         console.log(response)
     } catch (error) {
         console.log(error)
